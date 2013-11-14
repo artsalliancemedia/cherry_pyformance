767927d121522e0a101d252cac693b3d9beefd79
Danny Butler <danny.butler@artsalliancemedia.com>
2013-11-14 16:35:50 +0000

    Adding new database changes
    Removed Sender and Method Call tables.
    That information is now stored in key/value pairs in a Metadata table.

--- server/database.py
+++ server/database.py
@@ -6,58 +6,15 @@ Base = declarative_base()
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
 
-class Sender(Base):
-    __tablename__ = 'senders'
-    id = Column(Integer, primary_key=True)
-    ip_address = Column(String, unique=True)
-    exhibitor_chain = Column(String)
-    exhibitor_branch = Column(String)
-    product = Column(String)
-    version = Column(String)
-    
-    call_stacks = relationship("CallStack", cascade="all", backref='senders')
-    sql_statements = relationship("SQLStatement", cascade="all", backref='senders')
-  
-    def __init__(self, ip_address, stats_packet):
-        self.ip_address = ip_address
-        self.exhibitor_chain = stats_packet['exhibitor_chain']
-        self.exhibitor_branch = stats_packet['exhibitor_branch']
-        self.product = stats_packet['product']
-        self.version = stats_packet['version']
-      
-    def __repr__(self):
-        return self.ip_address
-
-class MethodCall(Base):
-    __tablename__ = 'method_calls'
-    id = Column(Integer, primary_key=True)
-    function = Column(String)
-    class_name = Column(String)
-    module = Column(String)
-    
-    call_stacks = relationship("CallStack", cascade="all", backref='method_calls')
-  
-    def __init__(self, profile_stats):
-        self.function = profile_stats['function']
-        self.class_name = profile_stats['class']
-        self.module = profile_stats['module']
-      
-    def __repr__(self):
-        return self.function
-
 class CallStack(Base):
     __tablename__ = 'call_stacks'
     id = Column(Integer, primary_key=True)
-    method_call_id = Column(None, ForeignKey('method_calls.id'))
-    sender_id = Column(None, ForeignKey('senders.id'))
     datetime = Column(Float)
     total_time = Column(Float)
     
     call_stack_items = relationship("CallStackItem", cascade="all", backref='call_stacks')
   
-    def __init__(self, method_call_id, sender_id, profile_stats):
-        self.method_call_id = method_call_id
-        self.sender_id = sender_id
+    def __init__(self, profile_stats):
         self.datetime = profile_stats['datetime']
         self.total_time = profile_stats['total_time']
       
@@ -92,19 +49,47 @@ class CallStackItem(Base):
 class SQLStatement(Base):
     __tablename__ = 'sql_statements'
     id = Column(Integer, primary_key=True)
-    sender_id = Column(None, ForeignKey('senders.id'))
     sql_string = Column(String)
     datetime = Column(Float)
     duration = Column(Float)
   
-    def __init__(self, sender_id, profile_stats):
-        self.sender_id = sender_id
+    def __init__(self, profile_stats):
         self.sql_string = profile_stats['sql']
         self.datetime = profile_stats['datetime']
         self.duration = profile_stats['duration']
       
     def __repr__(self):
         return self.sql_string
+
+class MetaData(Base):
+    __tablename__ = 'metadata'
+    id = Column(Integer, primary_key=True)
+    key = Column(String)
+    value = Column(String)
+  
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        
+class CallStackMetadata(Base):
+    __tablename__ = 'call_stack_metadata'
+    id = Column(Integer, primary_key=True)
+    call_stack_id = Column(None, ForeignKey('call_stacks.id'))
+    metadata_id = Column(None, ForeignKey('metadata.id'))
+  
+    def __init__(self, call_stack_id, metadata_id):
+        self.call_stack_id = call_stack_id
+        self.metadata_id = metadata_id
+        
+class SQLStatementMetadata(Base):
+    __tablename__ = 'sql_statement_metadata'
+    id = Column(Integer, primary_key=True)
+    sql_statement_id = Column(None, ForeignKey('sql_statements.id'))
+    metadata_id = Column(None, ForeignKey('metadata.id'))
+  
+    def __init__(self, sql_statement_id, metadata_id):
+        self.sql_statement_id = sql_statement_id
+        self.metadata_id = metadata_id
 
 def create_db_and_connect(postgres_string):
     database = sqlalchemy.create_engine(postgres_string + '/profile_stats')
@@ -131,48 +116,66 @@ def setup_profile_database(username, password, host, port):
     Session = sessionmaker(bind=db)
     global session
     session = Session()
-        
-def get_sender_id(ip, stats_packet):
-    sender_query = session.query(Sender).filter_by(ip_address=ip)
-    sender = None
-    if sender_query.count() == 0:
-        # Add new sender if does not exist
-        sender = Sender(ip, stats_packet)
-        session.add(sender)
-        session.commit()
-    else:
-        sender = sender_query.first()
-    return sender.id
 
-def get_method_call_id(profile_stats):
-    method_call_query = session.query(MethodCall).filter_by(function=profile_stats['function'], 
-                                                            class_name=profile_stats['class'], 
-                                                            module=profile_stats['module'])
-    method_call = None
-    if method_call_query.count() == 0:
-        # Add new sender if does not exist
-        method_call = MethodCall(profile_stats)
-        session.add(method_call)
-        session.commit()
-    else:
-        method_call = method_call_query.first()
-    return method_call.id
+sender_metadata_keys = ['exhibitor_chain','exhibitor_branch','product','version']
+method_call_metadata_keys = ['function','class','module']
+
+def get_metadata_list(metadata_dictionary, metadata_keys):
+    metadata_list = []
+    for metadata_key in metadata_keys:
+        metadata_query = session.query(MetaData).filter_by(key=metadata_key, value=metadata_dictionary[metadata_key])
+        if metadata_query.count() == 0:
+            # Add new metadata if does not exist
+            metadata = MetaData(metadata_key, metadata_dictionary[metadata_key])
+            metadata_list.append(metadata)
+            session.add(metadata)
+            session.commit()
+        else:
+            metadata_list.append(metadata_query.first())
+    return metadata_list
 
 def push_fn_stats(stats_packet, ip_address):
-    sender_id = get_sender_id(ip_address, stats_packet)
+    # Add sender metadata
+    sender_metadata_list = get_metadata_list(stats_packet, sender_metadata_keys)
+    for sender_metadata in sender_metadata_list:
+        session.add(sender_metadata)
+    
     for profile_stats in stats_packet['stats']:
-        method_call_id = get_method_call_id(profile_stats)
-        call_stack = CallStack(method_call_id, sender_id, profile_stats)
+        # Add function metadata
+        function_metadata_list = get_metadata_list(profile_stats, method_call_metadata_keys)
+        for function_metadata in function_metadata_list:
+            session.add(function_metadata)
+            
+        # Add call stack
+        call_stack = CallStack(profile_stats)
         session.add(call_stack)
         session.commit()
+        
+        # Add call stack/metadata relationships
+        for sender_metadata in sender_metadata_list:
+            session.add(CallStackMetadata(call_stack.id, sender_metadata.id))
+        for function_metadata in function_metadata_list:
+            session.add(CallStackMetadata(call_stack.id, function_metadata.id))
+        
+        # Add call stack items
         pstats = profile_stats['pstats']
         for stats in pstats:
             session.add(CallStackItem(call_stack.id, stats))
     session.commit()
 
 def push_sql_stats(stats_packet, ip_address):
-    sender_id = get_sender_id(ip_address, stats_packet)
+    # Add sender metadata
+    sender_metadata_list = get_metadata_list(stats_packet, sender_metadata_keys)
+    for sender_metadata in sender_metadata_list:
+        session.add(sender_metadata)
+    
     for sql_stat in stats_packet['stats']:
-        sql_statement = SQLStatement(sender_id, sql_stat)
+        # Add sql statement
+        sql_statement = SQLStatement(sql_stat)
         session.add(sql_statement)
+        session.commit()
+        
+        # Add sql statement/metadata relationships
+        for sender_metadata in sender_metadata_list:
+            session.add(SQLStatementMetadata(sql_statement.id, sender_metadata.id))
     session.commit()
