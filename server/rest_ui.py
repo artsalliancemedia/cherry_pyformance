51186c5b2780bee80015137e2e865b367649e8dc
alexanderson <alexander.anderson@artsalliancemedia.com>
2013-11-18 12:17:42 +0000

    Changed output of data to be more readable

--- server/rest_ui.py
+++ server/rest_ui.py
@@ -4,6 +4,7 @@ import mako.template
 import os
 from urllib import urlencode
 from cgi import escape as html_escape
+import time
 
 
 column_order = {'CallStack':['id','total_time','datetime'],
@@ -19,7 +20,14 @@ def json_get(table_class, id=None, **kwargs):
     items = db.session.query(table_class).filter_by(**kwargs).all()
     data = []
     for item in items:
-        record = [html_escape(str(item.__dict__[x])) for x in column_order[table_class.__name__]]
+        record = []
+        for column in column_order[table_class.__name__]:
+            datum = item.__dict__[column]
+            if column == 'datetime':
+                datum = time.strftime('%a, %d %b %Y %H:%M:%S', time.localtime(datum))
+            if type(datum) == float:
+                datum = "%f"%datum
+            record.append(html_escape(str(datum)))
         data.append(record)
     return {'aaData':data}
 
