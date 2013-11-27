733ee3b70acf68ab4dba2f950b09acd7cbb6d51d
alexanderson <alexander.anderson@artsalliancemedia.com>
2013-11-27 12:44:46 +0000

    updated basic individual sql statement page

--- server/json_ui.py
+++ server/json_ui.py
@@ -65,31 +65,3 @@ class JSONAPI(object):
             results = db.session.query(db.FileAccess)
             return [item._to_dict() for item in results.all()]
 
-
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def metadata(self, id=None, **kwargs):
-        main_table_item = None
-        if 'get_keys' in kwargs:
-            key_list_dicts = db.session.query(db.MetaData.key).distinct().all()
-            return [key_dict[0] for key_dict in key_list_dicts]
-        elif 'call_stack_id' in kwargs:
-            main_table_item = db.session.query(db.CallStack).get(kwargs['call_stack_id'])
-        elif 'sql_statement_id' in kwargs:
-            main_table_item = db.session.query(db.SQLStatement).get(kwargs['sql_statement_id'])
-        elif 'file_access_id' in kwargs:
-            main_table_item = db.session.query(db.FileAccess).get(kwargs['file_access_id'])
-        
-        data = []
-        if main_table_item:
-            for metadata in main_table_item.metadata_items:
-                record = [html_escape(str(metadata.__dict__[x])) for x in ['id','key','value']]
-                data.append(record)
-        else:
-            metadata_list = db.session.query(db.MetaData).filter_by(**kwargs).all()
-            for metadata in metadata_list:
-                value = html_escape(str(metadata.__dict__['value']))
-                data.append(value)
-            data.sort(key=str.lower)
-        
-        return {'aaData':data}
