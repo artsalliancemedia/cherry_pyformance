9fa56b94ed09a16edb446b52c6882136f6052eb4
Alex Latchford <alex.latchford@artsalliancemedia.com>
2013-12-13 12:14:54 +0000

    Fix a couple of regressions spotted by Danny and Alex.

--- server/aggregate_table_ui.py
+++ server/aggregate_table_ui.py
@@ -8,81 +8,81 @@ import database as db
 
 
 class AggregatePages(object):
-	@cherrypy.expose
-	def index(self):
-		return self.callstacks()
-	
-	templates_dir = os.path.join(os.getcwd(),'static','templates')
-	template_lookup = TemplateLookup(directories=[templates_dir,])
+    @cherrypy.expose
+    def index(self):
+        return self.callstacks()
+    
+    templates_dir = os.path.join(os.getcwd(),'static','templates')
+    template_lookup = TemplateLookup(directories=[templates_dir,])
 
-	@cherrypy.expose
-	def callstacks(self, id=None, **kwargs):
-		if id:
-			table_kwargs, filter_kwargs = parse_kwargs(kwargs)
-			call_stack, total, filtered = json_aggregate_item(id, filter_kwargs, db.CallStack)
+    @cherrypy.expose
+    def callstacks(self, id=None, **kwargs):
+        if id:
+            table_kwargs, filter_kwargs = parse_kwargs(kwargs)
+            call_stack, total, filtered = json_aggregate_item(db.CallStack, filter_kwargs, id)
 
-			if call_stack == None:
-				raise cherrypy.HTTPError(404)
+            if call_stack == None:
+                raise cherrypy.HTTPError(404)
 
-			call_stack[1] = str(call_stack[1]) #unicode throws off template when casting dict as js obj
-			call_stack[2] = int(call_stack[2]) #convert long to int
+            call_stack[1] = str(call_stack[1]) #unicode throws off template when casting dict as js obj
+            call_stack[2] = int(call_stack[2]) #convert long to int
 
-			if 'id' in filter_kwargs:
-				filter_kwargs.pop('id')
+            if 'id' in filter_kwargs:
+                filter_kwargs.pop('id')
 
-			for k in kwargs:
-				kwargs[k] = str(kwargs[k])
+            for k in kwargs:
+                kwargs[k] = str(kwargs[k])
 
-			mytemplate = Template(filename=os.path.join(self.templates_dir,'aggregatecallstack.html'), lookup=self.template_lookup)
-			return mytemplate.render(call_stack=call_stack, kwargs=filter_kwargs)
-		else:
-			mytemplate = Template(filename=os.path.join(self.templates_dir,'aggregatecallstacks.html'), lookup=self.template_lookup)
-			return mytemplate.render()
+            mytemplate = Template(filename=os.path.join(self.templates_dir,'aggregatecallstack.html'), lookup=self.template_lookup)
+            return mytemplate.render(call_stack=call_stack, kwargs=filter_kwargs)
+        else:
+            mytemplate = Template(filename=os.path.join(self.templates_dir,'aggregatecallstacks.html'), lookup=self.template_lookup)
+            return mytemplate.render()
 
-	@cherrypy.expose
-	def sqlstatements(self, id=None, **kwargs):
-		if id:
-			table_kwargs, filter_kwargs = parse_kwargs(kwargs)
-			statement, total, filtered = json_aggregate_item(id, filter_kwargs, db.SQLStatement)
+    @cherrypy.expose
+    def sqlstatements(self, id=None, **kwargs):
+        if id:
+            table_kwargs, filter_kwargs = parse_kwargs(kwargs)
+            statement, total, filtered = json_aggregate_item(db.SQLStatement, filter_kwargs, id)
 
-			if statement == None:
-				raise cherrypy.HTTPError(404)
+            if statement == None:
+                raise cherrypy.HTTPError(404)
 
-			statement[1] = str(statement[1]) #unicode throws off template when casting dict as js obj
-			statement[2] = int(statement[2]) #convert long to int
+            statement[1] = str(statement[1]) #unicode throws off template when casting dict as js obj
+            statement[2] = int(statement[2]) #convert long to int
 
-			if 'id' in filter_kwargs:
-				filter_kwargs.pop('id')
+            if 'id' in filter_kwargs:
+                filter_kwargs.pop('id')
 
-			for k in kwargs:
-				kwargs[k] = str(kwargs[k])
-			
-			mytemplate = Template(filename=os.path.join(self.templates_dir,'aggregatesql.html'), lookup=self.template_lookup)
-			return mytemplate.render(statement=statement, kwargs=filter_kwargs)
-		else:
-			mytemplate = Template(filename=os.path.join(self.templates_dir,'aggregatesqls.html'), lookup=self.template_lookup)
-			return mytemplate.render()
+            for k in kwargs:
+                kwargs[k] = str(kwargs[k])
+            
+            mytemplate = Template(filename=os.path.join(self.templates_dir,'aggregatesql.html'), lookup=self.template_lookup)
+            return mytemplate.render(statement=statement, kwargs=filter_kwargs)
+        else:
+            mytemplate = Template(filename=os.path.join(self.templates_dir,'aggregatesqls.html'), lookup=self.template_lookup)
+            return mytemplate.render()
 
-	@cherrypy.expose
-	def fileaccesses(self, id=None, **kwargs):
-		if id:
-			table_kwargs, filter_kwargs = parse_kwargs(kwargs)
-			file_access, total, filtered = json_aggregate_item(id, filter_kwargs, db.FileAccess)
+    @cherrypy.expose
+    def fileaccesses(self, id=None, **kwargs):
+        if id:
+            table_kwargs, filter_kwargs = parse_kwargs(kwargs)
+            file_access, total, filtered = json_aggregate_item(db.FileAccess, filter_kwargs, id)
 
-			if file_access == None:
-				raise cherrypy.HTTPError(404)
+            if file_access == None:
+                raise cherrypy.HTTPError(404)
 
-			file_access[1] = str(file_access[1]) #unicode throws off template when casting dict as js obj
-			file_access[2] = int(file_access[2]) #convert long to int
+            file_access[1] = str(file_access[1]) #unicode throws off template when casting dict as js obj
+            file_access[2] = int(file_access[2]) #convert long to int
 
-			if 'id' in filter_kwargs:
-				filter_kwargs.pop('id')
+            if 'id' in filter_kwargs:
+                filter_kwargs.pop('id')
 
-			for k in kwargs:
-				kwargs[k] = str(kwargs[k])
-			
-			mytemplate = Template(filename=os.path.join(self.templates_dir,'aggregatefileaccess.html'), lookup=self.template_lookup)
-			return mytemplate.render(file_access=file_access, kwargs=filter_kwargs)
-		else:
-			mytemplate = Template(filename=os.path.join(self.templates_dir,'aggregatefileaccesses.html'), lookup=self.template_lookup)
-			return mytemplate.render()
+            for k in kwargs:
+                kwargs[k] = str(kwargs[k])
+            
+            mytemplate = Template(filename=os.path.join(self.templates_dir,'aggregatefileaccess.html'), lookup=self.template_lookup)
+            return mytemplate.render(file_access=file_access, kwargs=filter_kwargs)
+        else:
+            mytemplate = Template(filename=os.path.join(self.templates_dir,'aggregatefileaccesses.html'), lookup=self.template_lookup)
+            return mytemplate.render()
