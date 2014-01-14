2b08fd6adad28fe37364216368834cb1b3176923
Maxwell Talbot <talbot.maxwell@gmail.com>
2014-01-14 17:11:27 +0000

    updated setup.py

--- setup/setup.py
+++ setup/setup.py
@@ -1,11 +1,13 @@
-from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
+from setuptools import setup
 
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-setup(name='cherry_pyformance',
-      version='0.1',
-      packages=['cherry_pyformance'],
-      data_files=[('cherry_pyformance', ['cherry_pyformance/default_config.cfg'])]
-     )
+setup(
+    name='cherry_pyformance',
+    version='0.1.1',
+    author = 'Arts Alliance Media',
+    author_email='development@artsalliancemedia.com',
+    url='http://www.artsalliancemedia.com',
+    packages=['cherry_pyformance'],
+    data_files=[
+        ('cherry_pyformance', ['cherry_pyformance/default_config.cfg'])
+    ]
+)
